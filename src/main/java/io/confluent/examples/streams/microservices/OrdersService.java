f50bcfcdf8f4ebae6542c9e576bacdbd6d369762
Ben Stopford <benstopford@gmail.com>
2018-05-11 10:43:32 +0100

    Tidy. No functional change

--- src/main/java/io/confluent/examples/streams/microservices/OrdersService.java
+++ src/main/java/io/confluent/examples/streams/microservices/OrdersService.java
@@ -17,6 +17,7 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.errors.InvalidStateStoreException;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Predicate;
 import org.apache.kafka.streams.state.QueryableStoreTypes;
@@ -61,23 +62,39 @@ import static org.apache.kafka.streams.state.StreamsMetadata.NOT_AVAILABLE;
 
 /**
  * This class provides a REST interface to write and read orders using a CQRS pattern
- * (https://martinfowler.com/bliki/CQRS.html). Three methods are exposed over HTTP: <p> -
- * POST(Order) -> Writes and order and returns location of the resource. <p> - GET(OrderId)
- * (Optional timeout) -> Returns requested order, blocking for timeout if no id present. <p> -
- * GET(OrderId)/Validated (Optional timeout) <p> POST does what you might expect: it adds an Order
- * to the system returning when Kafka sends the appropriate acknowledgement. <p> GET accesses an
- * inbuilt Materialized View, of Orders, which are kept in a State Store inside the service. This
- * CQRS-styled view is updated asynchronously wrt the HTTP POST. <p> Calling GET(id) when the ID is
- * not present will block the caller until either the order is added to the view, or the passed
- * TIMEOUT period elapses. This allows the caller to read-their-own-writes. <p> In addition HTTP
- * POST returns the location of the order submitted in the response. <p> Calling GET/id/validated
- * will block until the FAILED/VALIDATED order is available in the View. <p> The View can also be
- * scaled out linearly simply by adding more instances of the view service, and requests to any of
- * the REST endpoints will be automatically forwarded to the correct instance for the key requested
- * orderId via Kafka's Queryable State feature. <p> Non-blocking IO is used for all operations other
- * than the intialization of state stores on startup or rebalance which will block calling Jetty
- * thread. <p> NB This demo code only includes a partial implementation of the holding of
- * outstanding requests and as such would lead timeouts if used in a production use case.
+ * (https://martinfowler.com/bliki/CQRS.html). Three methods are exposed over HTTP:
+ * <p>
+ * - POST(Order) -> Writes and order and returns location of the resource.
+ * <p>
+ * - GET(OrderId) (Optional timeout) -> Returns requested order, blocking for timeout if no id present.
+ * <p>
+ * - GET(OrderId)/Validated (Optional timeout)
+ * <p>
+ * POST does what you might expect: it adds an Order to the system returning when Kafka sends the appropriate
+ * acknowledgement.
+ * <p>
+ * GET accesses an inbuilt Materialized View, of Orders, which are kept in a
+ * State Store inside the service. This CQRS-styled view is updated asynchronously wrt the HTTP
+ * POST.
+ * <p>
+ * Calling GET(id) when the ID is not present will block the caller until either the order
+ * is added to the view, or the passed TIMEOUT period elapses. This allows the caller to
+ * read-their-own-writes.
+ * <p>
+ * In addition HTTP POST returns the location of the order submitted in the response.
+ * <p>
+ * Calling GET/id/validated will block until the FAILED/VALIDATED order is available in
+ * the View.
+ * <p>
+ * The View can also be scaled out linearly simply by adding more instances of the
+ * view service, and requests to any of the REST endpoints will be automatically forwarded to the
+ * correct instance for the key requested orderId via Kafka's Queryable State feature.
+ * <p>
+ * Non-blocking IO is used for all operations other than the intialization of state stores on
+ * startup or rebalance which will block calling Jetty thread.
+ *<p>
+ * NB This demo code only includes a partial implementation of the holding of outstanding requests
+ * and as such would lead timeouts if used in a production use case.
  */
 @Path("v1")
 public class OrdersService implements Service {
@@ -163,7 +180,6 @@ public class OrdersService implements Service {
   }
 
   class FilteredResponse<K, V> {
-
     private AsyncResponse asyncResponse;
     private Predicate<K, V> predicate;
 
@@ -181,8 +197,7 @@ public class OrdersService implements Service {
    * @param predicate a filter that for this fetch, so for example we might fetch only VALIDATED
    * orders.
    */
-  private void fetchLocal(String id, AsyncResponse asyncResponse,
-      Predicate<String, Order> predicate) {
+  private void fetchLocal(String id, AsyncResponse asyncResponse, Predicate<String, Order> predicate) {
     log.info("running GET on this node");
     try {
       Order order = ordersStore().get(id);
@@ -203,10 +218,11 @@ public class OrdersService implements Service {
   }
 
   /**
-   * Use Kafka Streams' Queryable State API to work out if a key/value pair is located on this node,
-   * or on another Kafka Streams node. This returned HostStoreInfo can be used to redirect an HTTP
-   * request to the node that has the data. <p> If metadata is available, which can happen on
-   * startup, or during a rebalance, block until it is.
+   * Use Kafka Streams' Queryable State API to work out if a key/value pair is located on
+   * this node, or on another Kafka Streams node. This returned HostStoreInfo can be used
+   * to redirect an HTTP request to the node that has the data.
+   * <p>
+   * If metadata is available, which can happen on startup, or during a rebalance, block until it is.
    */
   private HostStoreInfo getKeyLocationOrBlock(String id, AsyncResponse asyncResponse) {
     HostStoreInfo locationOfKey;
@@ -376,8 +392,7 @@ public class OrdersService implements Service {
     final String restPort = args.length > 4 ? args[4] : null;
 
     Schemas.configureSerdesWithSchemaRegistryUrl(schemaRegistryUrl);
-    OrdersService service = new OrdersService(restHostname,
-        restPort == null ? 0 : Integer.valueOf(restPort));
+    OrdersService service = new OrdersService(restHostname, restPort == null ? 0 : Integer.valueOf(restPort));
     service.start(bootstrapServers, "/tmp/kafka-streams");
     addShutdownHookAndBlock(service);
   }
