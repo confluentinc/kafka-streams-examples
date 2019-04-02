49ffbd2b0ea6c6b7420e573ff52bc84830cbfc78
Jukka Karvanen <48978068+jukkakarvanen@users.noreply.github.com>
2019-04-02 14:38:22 +0300

    TopologyTestDriver based unit test, Refactored Sample to support UnitTest

--- src/main/java/io/confluent/examples/streams/WordCountLambdaExample.java
+++ src/main/java/io/confluent/examples/streams/WordCountLambdaExample.java
@@ -111,61 +111,20 @@ import java.util.regex.Pattern;
  */
 public class WordCountLambdaExample {
 
+  static final String inputTopic = "inputTopic";
+  static final String outputTopic = "outputTopic";
+
   public static void main(final String[] args) throws Exception {
     final String bootstrapServers = args.length > 0 ? args[0] : "localhost:9092";
-    final Properties streamsConfiguration = new Properties();
-    // Give the Streams application a unique name.  The name must be unique in the Kafka cluster
-    // against which the application is run.
-    streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-lambda-example");
-    streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "wordcount-lambda-example-client");
-    // Where to find Kafka broker(s).
-    streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-    // Specify default (de)serializers for record keys and for record values.
-    streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-    streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-    // Records should be flushed every 10 seconds. This is less than the default
-    // in order to keep this example interactive.
-    streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
-    // For illustrative purposes we disable record caches
-    streamsConfiguration.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
 
-    // Set up serializers and deserializers, which we will use for overriding the default serdes
-    // specified above.
-    final Serde<String> stringSerde = Serdes.String();
-    final Serde<Long> longSerde = Serdes.Long();
+    //Populate Stream Configuration
+    final Properties streamsConfiguration = getStreamsConfiguration(bootstrapServers);
 
     // In the subsequent lines we define the processing topology of the Streams application.
     final StreamsBuilder builder = new StreamsBuilder();
 
-    // Construct a `KStream` from the input topic "streams-plaintext-input", where message values
-    // represent lines of text (for the sake of this example, we ignore whatever may be stored
-    // in the message keys).
-    //
-    // Note: We could also just call `builder.stream("streams-plaintext-input")` if we wanted to leverage
-    // the default serdes specified in the Streams configuration above, because these defaults
-    // match what's in the actual topic.  However we explicitly set the deserializers in the
-    // call to `stream()` below in order to show how that's done, too.
-    final KStream<String, String> textLines = builder.stream("streams-plaintext-input");
-
-    final Pattern pattern = Pattern.compile("\\W+", Pattern.UNICODE_CHARACTER_CLASS);
-
-    final KTable<String, Long> wordCounts = textLines
-      // Split each text line, by whitespace, into words.  The text lines are the record
-      // values, i.e. we can ignore whatever data is in the record keys and thus invoke
-      // `flatMapValues()` instead of the more generic `flatMap()`.
-      .flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
-      // Count the occurrences of each word (record key).
-      //
-      // This will change the stream type from `KStream<String, String>` to `KTable<String, Long>`
-      // (word -> count).  In the `count` operation we must provide a name for the resulting KTable,
-      // which will be used to name e.g. its associated state store and changelog topic.
-      //
-      // Note: no need to specify explicit serdes because the resulting key and value types match our default serde settings
-      .groupBy((key, word) -> word)
-      .count();
-
-    // Write the `KTable<String, Long>` to the output topic.
-    wordCounts.toStream().to("streams-wordcount-output", Produced.with(stringSerde, longSerde));
+    //Create Actual Stream Processing pipeline
+    createWordCountStream(builder);
 
     // Now that we have finished the definition of the processing topology we can actually run
     // it via `start()`.  The Streams application as a whole can be launched just like any
@@ -186,6 +145,71 @@ public class WordCountLambdaExample {
 
     // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
     Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+  }
+
+  /** Populate Stream Configuration.
+   *
+   * @param bootstrapServers
+   * @return Properties getStreamsConfiguration
+   */
+  static Properties getStreamsConfiguration(String bootstrapServers) {
+    final Properties streamsConfiguration = new Properties();
+    // Give the Streams application a unique name.  The name must be unique in the Kafka cluster
+    // against which the application is run.
+    streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-lambda-example");
+    streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "wordcount-lambda-example-client");
+    // Where to find Kafka broker(s).
+    streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+    // Specify default (de)serializers for record keys and for record values.
+    streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+    streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+    // Records should be flushed every 10 seconds. This is less than the default
+    // in order to keep this example interactive.
+    streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
+    // For illustrative purposes we disable record caches
+    streamsConfiguration.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+    return streamsConfiguration;
+  }
+
+  /** Create Actual Stream Processing pipeline for Word Count.
+   *
+   * @param builder
+   */
+  static void createWordCountStream(StreamsBuilder builder) {
+    // Set up serializers and deserializers, which we will use for overriding the default serdes
+    // specified above.
+    final Serde<String> stringSerde = Serdes.String();
+    final Serde<Long> longSerde = Serdes.Long();
+
+    // Construct a `KStream` from the input topic "streams-plaintext-input", where message values
+    // represent lines of text (for the sake of this example, we ignore whatever may be stored
+    // in the message keys).
+    //
+    // Note: We could also just call `builder.stream("streams-plaintext-input")` if we wanted to leverage
+    // the default serdes specified in the Streams configuration above, because these defaults
+    // match what's in the actual topic.  However we explicitly set the deserializers in the
+    // call to `stream()` below in order to show how that's done, too.
+    final KStream<String, String> textLines = builder.stream(inputTopic);
+
+    final Pattern pattern = Pattern.compile("\\W+", Pattern.UNICODE_CHARACTER_CLASS);
+
+    final KTable<String, Long> wordCounts = textLines
+      // Split each text line, by whitespace, into words.  The text lines are the record
+      // values, i.e. we can ignore whatever data is in the record keys and thus invoke
+      // `flatMapValues()` instead of the more generic `flatMap()`.
+      .flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
+      // Count the occurrences of each word (record key).
+      //
+      // This will change the stream type from `KStream<String, String>` to `KTable<String, Long>`
+      // (word -> count).  In the `count` operation we must provide a name for the resulting KTable,
+      // which will be used to name e.g. its associated state store and changelog topic.
+      //
+      // Note: no need to specify explicit serdes because the resulting key and value types match our default serde settings
+      .groupBy((key, word) -> word)
+      .count();
+
+    // Write the `KTable<String, Long>` to the output topic.
+    wordCounts.toStream().to(outputTopic, Produced.with(stringSerde, longSerde));
   }
 
 }
---
+++ src/test/java/io/confluent/examples/streams/WordCountLambdaExampleTest.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright Confluent Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.confluent.examples.streams;
+
+import io.confluent.examples.streams.kafka.EmbeddedSingleNodeKafkaCluster;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.*;
+import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.test.ConsumerRecordFactory;
+import org.apache.kafka.streams.test.OutputVerifier;
+import org.apache.kafka.test.TestUtils;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.*;
+
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Stream processing unit test of {@link WordCountLambdaExample}, using TopologyTestDriver.
+ *
+ * See {@link WordCountLambdaExample} for further documentation.
+ *
+ * See {@link WordCountLambdaIntegrationTest} for the End-to-end integration test using an embedded Kafkacluster.
+ *
+ * Note: This example uses lambda expressions and thus works with Java 8+ only.
+ */
+public class WordCountLambdaExampleTest {
+
+  private TopologyTestDriver testDriver;
+
+  private StringDeserializer stringDeserializer = new StringDeserializer();
+  private LongDeserializer longDeserializer = new LongDeserializer();
+  private ConsumerRecordFactory<String, String> recordFactory = new ConsumerRecordFactory<>(new StringSerializer(), new StringSerializer());
+
+  @Before
+  public void setup() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
+    final StreamsBuilder builder = new StreamsBuilder();
+    //Create Actual Stream Processing pipeline
+    WordCountLambdaExample.createWordCountStream(builder);
+    testDriver = new TopologyTestDriver(builder.build(), WordCountLambdaExample.getStreamsConfiguration("localhost:9092"));
+  }
+
+  @After
+  public void tearDown() {
+    try {
+      testDriver.close();
+    } catch (RuntimeException e) {
+      // https://issues.apache.org/jira/browse/KAFKA-6647 causes exception when executed in Windows, ignoring it
+      // Logged stacktrace cannot be avoided
+      System.out.println("Ignoring exception, test failing in Windows due this exception:" + e.getLocalizedMessage());
+    }
+  }
+
+  private Map<String, Long> getOutputList() {
+    Map<String, Long> output = new HashMap<>();
+    ProducerRecord<String, Long> outputRow;
+    while((outputRow = testDriver.readOutput(WordCountLambdaExample.outputTopic, stringDeserializer, longDeserializer)) != null) {
+      output.put(outputRow.key(), outputRow.value());
+    }
+    return output;
+  }
+
+  /** Simple test validating count of one word
+   */
+  @Test
+  public void testOneWord() {
+    String nullKey = null;
+    testDriver.pipeInput(recordFactory.create(WordCountLambdaExample.inputTopic, nullKey, "Hello", 1L));
+    ProducerRecord<String, Long> output = testDriver.readOutput(WordCountLambdaExample.outputTopic, stringDeserializer, longDeserializer);
+    OutputVerifier.compareKeyValue(output, "hello", 1L);
+  }
+
+  @Test
+  public void shouldCountWords() throws Exception {
+    final List<String> inputValues = Arrays.asList(
+        "Hello Kafka Streams",
+        "All streams lead to Kafka",
+        "Join Kafka Summit",
+        "И теперь пошли русские слова"
+    );
+    final Map<String, Long> expectedWordCounts = new HashMap<>();
+    expectedWordCounts.put("hello", 1L);
+    expectedWordCounts.put("all", 1L);
+    expectedWordCounts.put("streams", 2L);
+    expectedWordCounts.put("lead", 1L);
+    expectedWordCounts.put("to", 1L);
+    expectedWordCounts.put("join", 1L);
+    expectedWordCounts.put("kafka", 3L);
+    expectedWordCounts.put("summit", 1L);
+    expectedWordCounts.put("и", 1L);
+    expectedWordCounts.put("теперь", 1L);
+    expectedWordCounts.put("пошли", 1L);
+    expectedWordCounts.put("русские", 1L);
+    expectedWordCounts.put("слова", 1L);
+
+    List<KeyValue<String, String>> records = inputValues.stream().map(v -> new KeyValue<String, String>(null, v)).collect(Collectors.toList());
+    testDriver.pipeInput(recordFactory.create(WordCountLambdaExample.inputTopic, records, 1L, 100L));
+
+    final Map<String, Long> actualWordCounts = getOutputList();
+    assertThat(actualWordCounts).containsAllEntriesOf(expectedWordCounts);
+  }
+
+}
