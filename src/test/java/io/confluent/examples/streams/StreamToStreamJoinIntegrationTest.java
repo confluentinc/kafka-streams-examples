461cb1899f2d74cef9bf359c2fb92f5e16b3e9a4
John Roesler <vvcephei@users.noreply.github.com>
2019-05-13 15:13:27 -0500

    MINOR: port more tests to TopologyTestDriver (#248)
    Continue the work of #242 , #245 , and #246 by porting the rest of the integration tests that don't use Schema Registry.
    
    The Schema Registry tests will get ported in a later PR.

--- src/test/java/io/confluent/examples/streams/StreamToStreamJoinIntegrationTest.java
+++ src/test/java/io/confluent/examples/streams/StreamToStreamJoinIntegrationTest.java
@@ -54,44 +54,39 @@ public class StreamToStreamJoinIntegrationTest {
   private static final String outputTopic = "output-topic";
 
   @Test
-  public void shouldJoinTwoStreams() throws Exception {
+  public void shouldJoinTwoStreams() {
     // Input 1: Ad impressions
     final List<KeyValue<String, String>> inputAdImpressions = Arrays.asList(
-        new KeyValue<>("car-advertisement", "shown"),
-        new KeyValue<>("newspaper-advertisement", "shown"),
-        new KeyValue<>("gadget-advertisement", "shown")
+      new KeyValue<>("car-advertisement", "shown"),
+      new KeyValue<>("newspaper-advertisement", "shown"),
+      new KeyValue<>("gadget-advertisement", "shown")
     );
 
     // Input 2: Ad clicks
     final List<KeyValue<String, String>> inputAdClicks = Arrays.asList(
-        new KeyValue<>("newspaper-advertisement", "clicked"),
-        new KeyValue<>("gadget-advertisement", "clicked"),
-        new KeyValue<>("newspaper-advertisement", "clicked")
+      new KeyValue<>("newspaper-advertisement", "clicked"),
+      new KeyValue<>("gadget-advertisement", "clicked"),
+      new KeyValue<>("newspaper-advertisement", "clicked")
     );
 
     final List<KeyValue<String, String>> expectedResults = Arrays.asList(
-        new KeyValue<>("car-advertisement", "shown/null"),
-        new KeyValue<>("newspaper-advertisement", "shown/null"),
-        new KeyValue<>("gadget-advertisement", "shown/null"),
-        new KeyValue<>("newspaper-advertisement", "shown/clicked"),
-        new KeyValue<>("gadget-advertisement", "shown/clicked"),
-        new KeyValue<>("newspaper-advertisement", "shown/clicked")
+      new KeyValue<>("car-advertisement", "shown/null"),
+      new KeyValue<>("newspaper-advertisement", "shown/null"),
+      new KeyValue<>("gadget-advertisement", "shown/null"),
+      new KeyValue<>("newspaper-advertisement", "shown/clicked"),
+      new KeyValue<>("gadget-advertisement", "shown/clicked"),
+      new KeyValue<>("newspaper-advertisement", "shown/clicked")
     );
 
     //
     // Step 1: Configure and start the processor topology.
     //
-    final Serde<String> stringSerde = Serdes.String();
 
     final Properties streamsConfiguration = new Properties();
     streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "stream-stream-join-lambda-integration-test");
     streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy config");
     streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
     streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-    // The commit interval for flushing records to state stores and downstream must be lower than
-    // this integration test's timeout (30 secs) to ensure we observe the expected processing results.
-    streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
-    streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
     // Use a temporary directory for storing state, which will be automatically removed after the test.
     streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG, TestUtils.tempDirectory().getAbsolutePath());
 
@@ -104,48 +99,52 @@ public class StreamToStreamJoinIntegrationTest {
     // for the same join key (e.g. "newspaper-advertisement"), we receive an update from either of
     // the two joined streams during the defined join window.
     final KStream<String, String> impressionsAndClicks = alerts.outerJoin(incidents,
-        (impressionValue, clickValue) -> impressionValue + "/" + clickValue,
-        // KStream-KStream joins are always windowed joins, hence we must provide a join window.
-        JoinWindows.of(TimeUnit.SECONDS.toMillis(5)));
+      (impressionValue, clickValue) -> impressionValue + "/" + clickValue,
+      // KStream-KStream joins are always windowed joins, hence we must provide a join window.
+      JoinWindows.of(TimeUnit.SECONDS.toMillis(5)));
 
     // Write the results to the output topic.
     impressionsAndClicks.to(outputTopic);
 
     final TopologyTestDriver topologyTestDriver = new TopologyTestDriver(builder.build(), streamsConfiguration);
 
-    //
-    // Step 2: Publish ad impressions.
-    //
-    IntegrationTestUtils.produceKeyValuesSynchronously(
-      adImpressionsTopic,
-      inputAdImpressions,
-      topologyTestDriver,
-      new StringSerializer(),
-      new StringSerializer()
-    );
-
-    //
-    // Step 3: Publish ad clicks.
-    //
-    IntegrationTestUtils.produceKeyValuesSynchronously(
-      adClicksTopic,
-      inputAdClicks,
-      topologyTestDriver,
-      new StringSerializer(),
-      new StringSerializer()
-    );
-
-    //
-    // Step 4: Verify the application's output data.
-    //
-    final List<KeyValue<String, String>> actualResults =
-      IntegrationTestUtils.drainStreamOutput(
-        outputTopic,
+    try {
+      //
+      // Step 2: Publish ad impressions.
+      //
+      IntegrationTestUtils.produceKeyValuesSynchronously(
+        adImpressionsTopic,
+        inputAdImpressions,
         topologyTestDriver,
-        new StringDeserializer(),
-        new StringDeserializer()
+        new StringSerializer(),
+        new StringSerializer()
       );
-    assertThat(actualResults).containsExactlyElementsOf(expectedResults);
+
+      //
+      // Step 3: Publish ad clicks.
+      //
+      IntegrationTestUtils.produceKeyValuesSynchronously(
+        adClicksTopic,
+        inputAdClicks,
+        topologyTestDriver,
+        new StringSerializer(),
+        new StringSerializer()
+      );
+
+      //
+      // Step 4: Verify the application's output data.
+      //
+      final List<KeyValue<String, String>> actualResults =
+        IntegrationTestUtils.drainStreamOutput(
+          outputTopic,
+          topologyTestDriver,
+          new StringDeserializer(),
+          new StringDeserializer()
+        );
+      assertThat(actualResults).containsExactlyElementsOf(expectedResults);
+    } finally {
+      topologyTestDriver.close();
+    }
   }
 
 }
--- src/test/scala/io/confluent/examples/streams/ProbabilisticCountingScalaIntegrationTest.scala
+++ src/test/scala/io/confluent/examples/streams/ProbabilisticCountingScalaIntegrationTest.scala
@@ -18,17 +18,17 @@ package io.confluent.examples.streams
 import java.util
 import java.util.Properties
 
+import io.confluent.examples.streams.IntegrationTestUtils.NothingSerde
 import io.confluent.examples.streams.algebird.{CMSStore, CMSStoreBuilder, ProbabilisticCounter}
-import io.confluent.examples.streams.kafka.EmbeddedSingleNodeKafkaCluster
-import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization._
+import org.apache.kafka.streams._
 import org.apache.kafka.streams.kstream.{KStream, Produced, TransformerSupplier}
-import org.apache.kafka.streams.{KafkaStreams, KeyValue, StreamsBuilder, StreamsConfig}
 import org.apache.kafka.test.TestUtils
 import org.assertj.core.api.Assertions.assertThat
 import org.junit._
 import org.scalatest.junit.AssertionsForJUnit
+
+import scala.collection.JavaConverters._
 
 /**
   * End-to-end integration test that demonstrates how to probabilistically count items in an input stream.
@@ -38,23 +38,12 @@ import org.scalatest.junit.AssertionsForJUnit
   */
 class ProbabilisticCountingScalaIntegrationTest extends AssertionsForJUnit {
 
-  private val privateCluster: EmbeddedSingleNodeKafkaCluster = new EmbeddedSingleNodeKafkaCluster
-
-  @Rule def cluster: EmbeddedSingleNodeKafkaCluster = privateCluster
-
   private val inputTopic = "inputTopic"
   private val outputTopic = "output-topic"
-
-  @Before
-  def startKafkaCluster() {
-    cluster.createTopic(inputTopic)
-    cluster.createTopic(outputTopic)
-  }
 
   @Test
   def shouldProbabilisticallyCountWords() {
     // To convert between Scala's `Tuple2` and Streams' `KeyValue`.
-    import KeyValueImplicits._
 
     val inputTextLines: Seq[String] = Seq(
       "Hello Kafka Streams",
@@ -62,7 +51,7 @@ class ProbabilisticCountingScalaIntegrationTest extends AssertionsForJUnit {
       "Join Kafka Summit"
     )
 
-    val expectedWordCounts: Seq[KeyValue[String, Long]] = Seq(
+    val expectedWordCounts: Map[String, Long] = Map(
       ("hello", 1L),
       ("kafka", 1L),
       ("streams", 1L),
@@ -82,13 +71,9 @@ class ProbabilisticCountingScalaIntegrationTest extends AssertionsForJUnit {
     val streamsConfiguration: Properties = {
       val p = new Properties()
       p.put(StreamsConfig.APPLICATION_ID_CONFIG, "probabilistic-counting-scala-integration-test")
-      p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, cluster.bootstrapServers())
+      p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy config")
       p.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.ByteArray.getClass.getName)
       p.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String.getClass.getName)
-      // The commit interval for flushing records to state stores and downstream must be lower than
-      // this integration test's timeout (30 secs) to ensure we observe the expected processing results.
-      p.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, "10000")
-      p.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
       // Use a temporary directory for storing state, which will be automatically removed after the test.
       p.put(StreamsConfig.STATE_DIR_CONFIG, TestUtils.tempDirectory.getAbsolutePath)
       p
@@ -99,63 +84,52 @@ class ProbabilisticCountingScalaIntegrationTest extends AssertionsForJUnit {
     builder.addStateStore(createCMSStoreBuilder(cmsStoreName))
 
     // Read the input from Kafka.
-    val textLines: KStream[Array[Byte], String] = builder.stream(inputTopic)
+    val textLines: KStream[String, String] = builder.stream(inputTopic)
 
     // Scala-Java interoperability: to convert `scala.collection.Iterable` to  `java.util.Iterable`
     // in `flatMapValues()` below.
     import collection.JavaConverters.asJavaIterableConverter
 
     val approximateWordCounts: KStream[String, Long] = textLines
-        .flatMapValues(textLine => textLine.toLowerCase.split("\\W+").toIterable.asJava)
-        .transform(
-          new TransformerSupplier[Array[Byte], String, KeyValue[String, Long]] {
-            override def get() = new ProbabilisticCounter(cmsStoreName)
-          },
-          cmsStoreName)
+      .flatMapValues(textLine => textLine.toLowerCase.split("\\W+").toIterable.asJava)
+      .transform(
+        new TransformerSupplier[String, String, KeyValue[String, Long]] {
+          override def get() = new ProbabilisticCounter(cmsStoreName)
+        },
+        cmsStoreName)
 
     // Trick to re-use Kafka's serde for java.lang.Long for scala.Long.
     val longSerde: Serde[Long] = Serdes.Long().asInstanceOf[Serde[Long]]
     // Write the results back to Kafka.
     approximateWordCounts.to(outputTopic, Produced.`with`(Serdes.String(), longSerde))
 
-    val streams: KafkaStreams = new KafkaStreams(builder.build(), streamsConfiguration)
-    streams.start()
+    val topologyTestDriver = new TopologyTestDriver(builder.build(), streamsConfiguration)
 
-    //
-    // Step 2: Publish some input text lines.
-    //
-    val producerConfig: Properties = {
-      val p = new Properties()
-      p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, cluster.bootstrapServers())
-      p.put(ProducerConfig.ACKS_CONFIG, "all")
-      p.put(ProducerConfig.RETRIES_CONFIG, "0")
-      p.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[ByteArraySerializer])
-      p.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
-      p
+    try {
+      //
+      // Step 2: Publish some input text lines.
+      //
+      IntegrationTestUtils.produceKeyValuesSynchronously(
+        inputTopic,
+        inputTextLines.map(v => new KeyValue(null, v)).asJava,
+        topologyTestDriver,
+        new NothingSerde[Null],
+        new StringSerializer
+      )
+
+      //
+      // Step 3: Verify the application's output data.
+      //
+      val actualWordCounts =
+      IntegrationTestUtils.drainTableOutput(outputTopic, topologyTestDriver, new StringDeserializer, new LongDeserializer)
+
+      // Note: This example only processes a small amount of input data, for which the word counts
+      // will actually be exact counts.  However, for large amounts of input data we would expect to
+      // observe approximate counts (where the approximate counts would be >= true exact counts).
+      assertThat(actualWordCounts).isEqualTo(expectedWordCounts.asJava)
+    } finally {
+      topologyTestDriver.close()
     }
-    import collection.JavaConverters._
-    IntegrationTestUtils.produceValuesSynchronously(inputTopic, inputTextLines.asJava, producerConfig)
-
-    //
-    // Step 3: Verify the application's output data.
-    //
-    val consumerConfig = {
-      val p = new Properties()
-      p.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, cluster.bootstrapServers())
-      p.put(ConsumerConfig.GROUP_ID_CONFIG, "probabilistic-counting-consumer")
-      p.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
-      p.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
-      p.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[LongDeserializer])
-      p
-    }
-    val actualWordCounts: java.util.List[KeyValue[String, Long]] =
-      IntegrationTestUtils.waitUntilMinKeyValueRecordsReceived(consumerConfig, outputTopic, expectedWordCounts.size)
-    streams.close()
-
-    // Note: This example only processes a small amount of input data, for which the word counts
-    // will actually be exact counts.  However, for large amounts of input data we would expect to
-    // observe approximate counts (where the approximate counts would be >= true exact counts).
-    assertThat(actualWordCounts).containsExactlyElementsOf(expectedWordCounts.asJava)
   }
 
   private def createCMSStoreBuilder(cmsStoreName: String): CMSStoreBuilder[String] = {
@@ -168,7 +142,7 @@ class ProbabilisticCountingScalaIntegrationTest extends AssertionsForJUnit {
       cfg
     }
     new CMSStoreBuilder[String](cmsStoreName, Serdes.String())
-        .withLoggingEnabled(changelogConfig)
+      .withLoggingEnabled(changelogConfig)
   }
 
 }
