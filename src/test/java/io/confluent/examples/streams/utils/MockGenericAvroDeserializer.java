65c7e025630cedec2116b9aa669a016d5092f211
John Roesler <john@confluent.io>
2019-05-15 17:03:05 -0500

    MINOR: migrate GenericAvroIntegrationTest to TTD

---
+++ src/test/java/io/confluent/examples/streams/utils/MockGenericAvroDeserializer.java
@@ -0,0 +1,30 @@
+package io.confluent.examples.streams.utils;
+
+import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.util.Map;
+
+public class MockGenericAvroDeserializer implements Deserializer<GenericRecord> {
+  private KafkaAvroDeserializer inner;
+
+  @Override
+  public void configure(final Map<String, ?> configs, final boolean isKey) {
+    final MockSchemaRegistryClient mockSchemaRegistryClient =
+      (MockSchemaRegistryClient) configs.get("mock.schema.registry.client");
+    inner = new KafkaAvroDeserializer(mockSchemaRegistryClient);
+    inner.configure(configs, isKey);
+  }
+
+  @Override
+  public GenericRecord deserialize(final String topic, final byte[] data) {
+    return (GenericRecord) inner.deserialize(topic, data);
+  }
+
+  @Override
+  public void close() {
+    inner.close();
+  }
+}
