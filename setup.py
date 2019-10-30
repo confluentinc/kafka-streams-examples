47288a4b990584e1c30485c5afbdd8bbb00ebda2
Mohinish <mohinish@confluent.io>
2019-10-30 03:08:48 -0700

    Bump Confluent to 5.4.0-SNAPSHOT, Kafka to 5.4.0-SNAPSHOT

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='kafka-streams-examples-tests',
-    version='3.3.0',
+    version='5.4.0-SNAPSHOT',
 
     author="Confluent, Inc.",
     author_email="tools@confluent.io",
