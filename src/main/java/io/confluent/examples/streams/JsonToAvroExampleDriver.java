7b74cbfdc3b8cb1b02dd80a3018d833a0414fcbd
Bill Bejeck <bill@confluent.io>
2020-01-09 21:07:07 -0500

    MINOR: Change Avro dependency required for schema registry updates

--- src/main/java/io/confluent/examples/streams/JsonToAvroExampleDriver.java
+++ src/main/java/io/confluent/examples/streams/JsonToAvroExampleDriver.java
@@ -15,17 +15,11 @@
  */
 package io.confluent.examples.streams;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.confluent.examples.streams.avro.WikiFeed;
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroDeserializer;
-import java.io.IOException;
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -35,7 +29,10 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.*;
 
 /**
  * This is a sample driver for the {@link JsonToAvroExample} and
