d9e39fe32c9427060b589fce781e73a762d3c7d4
Andrew Egelhofer <aegelhofer@confluent.io>
2020-09-18 14:30:33 -0700

    Set Confluent to 6.1.0-beta200825192044-cp2, Kafka to 6.1.0-beta200825192044-cp2.

--- src/main/java/io/confluent/examples/streams/WordCountLambdaExample.java
+++ src/main/java/io/confluent/examples/streams/WordCountLambdaExample.java
@@ -62,7 +62,7 @@ import java.util.regex.Pattern;
  * Once packaged you can then run:
  * <pre>
  * {@code
- * $ java -cp target/kafka-streams-examples-6.1.0-beta200825192044-standalone.jar io.confluent.examples.streams.WordCountLambdaExample
+ * $ java -cp target/kafka-streams-examples-6.1.0-beta200825192044-cp2-standalone.jar io.confluent.examples.streams.WordCountLambdaExample
  * }
  * </pre>
  * 4) Write some input data to the source topic "streams-plaintext-input" (e.g. via {@code kafka-console-producer}).
