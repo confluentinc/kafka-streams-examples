f8f425d661d06f48df215ea32cf284f676ad4c96
ConfluentSemaphore <40306929+ConfluentSemaphore@users.noreply.github.com>
2024-07-03 10:41:19 +0000

    Set Confluent to 7.7.0, Kafka to 7.7.0.

--- src/main/java/io/confluent/examples/streams/interactivequeries/WordCountInteractiveQueriesExample.java
+++ src/main/java/io/confluent/examples/streams/interactivequeries/WordCountInteractiveQueriesExample.java
@@ -81,7 +81,7 @@ import java.util.Properties;
  *
  * <pre>
  * {@code
- * $ java -cp target/kafka-streams-examples-7.7.0-0-standalone.jar \
+ * $ java -cp target/kafka-streams-examples-7.7.0-standalone.jar \
  *      io.confluent.examples.streams.interactivequeries.WordCountInteractiveQueriesExample 7070
  * }
  * </pre>
@@ -92,7 +92,7 @@ import java.util.Properties;
  *
  * <pre>
  * {@code
- * $ java -cp target/kafka-streams-examples-7.7.0-0-standalone.jar \
+ * $ java -cp target/kafka-streams-examples-7.7.0-standalone.jar \
  *      io.confluent.examples.streams.interactivequeries.WordCountInteractiveQueriesExample 7071
  * }
  * </pre>
