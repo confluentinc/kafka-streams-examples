c819818d6b458c8012fe46fc2c36bedab81b44eb
ConfluentSemaphore <40306929+ConfluentSemaphore@users.noreply.github.com>
2024-08-28 05:16:59 +0000

    Set Confluent to 7.5.6, Kafka to 7.5.6.

--- src/main/java/io/confluent/examples/streams/SessionWindowsExampleDriver.java
+++ src/main/java/io/confluent/examples/streams/SessionWindowsExampleDriver.java
@@ -42,7 +42,7 @@ import java.util.Properties;
  * Once packaged you can then run:
  * <pre>
  * {@code
- * $ java -cp target/kafka-streams-examples-7.5.6-0-standalone.jar io.confluent.examples.streams.SessionWindowsExampleDriver
+ * $ java -cp target/kafka-streams-examples-7.5.6-standalone.jar io.confluent.examples.streams.SessionWindowsExampleDriver
  * }
  * </pre>
  */
