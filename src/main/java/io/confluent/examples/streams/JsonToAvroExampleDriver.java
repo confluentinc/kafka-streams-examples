ca70102df28e1dfad5cba924b91363463f4472da
ConfluentSemaphore <40306929+ConfluentSemaphore@users.noreply.github.com>
2024-08-28 05:16:49 +0000

    Set Confluent to 7.1.14, Kafka to 7.1.14.

--- src/main/java/io/confluent/examples/streams/JsonToAvroExampleDriver.java
+++ src/main/java/io/confluent/examples/streams/JsonToAvroExampleDriver.java
@@ -49,7 +49,7 @@ import java.io.IOException;
  * Once packaged you can then run:
  * <pre>
  * {@code
- * java -cp target/kafka-streams-examples-7.1.14-0-standalone.jar io.confluent.examples.streams.JsonToAvroExampleDriver
+ * java -cp target/kafka-streams-examples-7.1.14-standalone.jar io.confluent.examples.streams.JsonToAvroExampleDriver
  * }
  * </pre>
  *
