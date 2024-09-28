0bf551c17fb64dff744f509a49e46dbe12cb79b9
ConfluentSemaphore <40306929+ConfluentSemaphore@users.noreply.github.com>
2024-09-28 15:22:33 +0000

    Set Confluent to 7.7.0-cp3, Kafka to 7.7.0-cp3.

--- src/main/java/io/confluent/examples/streams/TopArticlesExampleDriver.java
+++ src/main/java/io/confluent/examples/streams/TopArticlesExampleDriver.java
@@ -52,7 +52,7 @@ import java.util.stream.IntStream;
  * Once packaged you can then run:
  * <pre>
  * {@code
- * java -cp target/kafka-streams-examples-7.7.0-standalone.jar io.confluent.examples.streams.TopArticlesLambdaExample
+ * java -cp target/kafka-streams-examples-7.7.0-cp3-standalone.jar io.confluent.examples.streams.TopArticlesLambdaExample
  * }
  * </pre>
  *
