f6d5ae15ac79d66451779dfae9ec9473a2fb96bc
ConfluentSemaphore <40306929+ConfluentSemaphore@users.noreply.github.com>
2024-12-05 06:22:13 +0000

    Bump Confluent to 7.8.1-0, Kafka to 7.8.1-0

--- src/main/scala/io/confluent/examples/streams/MapFunctionScalaExample.scala
+++ src/main/scala/io/confluent/examples/streams/MapFunctionScalaExample.scala
@@ -54,7 +54,7 @@ import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
   * Once packaged you can then run:
   *
   * {{{
-  * $ java -cp target/kafka-streams-examples-7.8.0-0-standalone.jar io.confluent.examples.streams.MapFunctionScalaExample
+  * $ java -cp target/kafka-streams-examples-7.8.1-0-standalone.jar io.confluent.examples.streams.MapFunctionScalaExample
   * }}}
   *
   * 4) Write some input data to the source topics (e.g. via `kafka-console-producer`.  The already
