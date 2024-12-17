52c43e05684ffd855b9ace79a34a583a6170b0fb
ConfluentSemaphore <40306929+ConfluentSemaphore@users.noreply.github.com>
2024-12-17 17:19:10 +0000

    Set Confluent to 7.5.2-cp3, Kafka to 7.5.2-cp3.

--- src/main/java/io/confluent/examples/streams/interactivequeries/kafkamusic/KafkaMusicExample.java
+++ src/main/java/io/confluent/examples/streams/interactivequeries/kafkamusic/KafkaMusicExample.java
@@ -110,7 +110,7 @@ import static java.util.Collections.singletonMap;
  *
  * <pre>
  * {@code
- * $ java -cp target/kafka-streams-examples-7.5.2-standalone.jar \
+ * $ java -cp target/kafka-streams-examples-7.5.2-cp3-standalone.jar \
  *      io.confluent.examples.streams.interactivequeries.kafkamusic.KafkaMusicExample 7070
  * }
  * </pre>
@@ -121,7 +121,7 @@ import static java.util.Collections.singletonMap;
  *
  * <pre>
  * {@code
- * $ java -cp target/kafka-streams-examples-7.5.2-standalone.jar \
+ * $ java -cp target/kafka-streams-examples-7.5.2-cp3-standalone.jar \
  *      io.confluent.examples.streams.interactivequeries.kafkamusic.KafkaMusicExample 7071
  * }
  * </pre>
